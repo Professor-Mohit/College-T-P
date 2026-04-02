c472e4bc18fe5d6347398fa0168655b4606cdb20
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-02 21:03:08 +0530

    Counting the occerence of the number using of target

---
+++ Arrays/count_occerence_Specidic_elem.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class count_occerence_Specidic_elem {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 3, 3};
+        int target = 3;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+        System.out.println("Element " + target + " occurs " + count + " times in the array.");
+    }
+}

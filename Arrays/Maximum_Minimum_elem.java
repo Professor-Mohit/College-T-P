a77f6631ef8805c92c43dcd3f78b4e4e1232da0c
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-03 16:02:04 +0530

    Maximum and minimum

--- Arrays/Maximum_Minimum_elem.java
+++ Arrays/Maximum_Minimum_elem.java
@@ -7,7 +7,7 @@ public class Maximum_Minimum_elem {
         int max = arr[0];
         int min = arr[0];
 
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }

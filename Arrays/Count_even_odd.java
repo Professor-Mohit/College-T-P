fc6642829f06c6be3962cbeed7d44354405abdb1
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-09 20:42:51 +0530

    Counting even and odd numbers

---
+++ Arrays/Count_even_odd.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class Count_even_odd {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Number of even elements in the array: " + evenCount);
+        System.out.println("Number of odd elements in the array: " + oddCount);
+    }
+}

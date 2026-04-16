b5d509ce192ab4fb0afb47ec84f8c228ed806f5e
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-16 23:41:22 +0530

    Remove all the duplicates

---
+++ Arrays/Remove_duplicates.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class Remove_duplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        int n = arr.length;
+        if (n == 0 || n == 1) {
+            System.out.println("Array has no duplicates.");
+            return;
+        }
+
+        int[] temp = new int[n];
+        int j = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temp[j++] = arr[i];
+            }
+        }
+        temp[j++] = arr[n - 1];
+
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < j; i++) {
+            System.out.print(temp[i] + " ");
+        }
+    }
+}

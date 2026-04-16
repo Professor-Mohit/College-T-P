e7578e0e1a9e6bea3545b6d9f705e7e07f9d30ab
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-17 00:29:37 +0530

    Roataing kth element in the left

---
+++ Arrays/Rotate_K_left.java
@@ -0,0 +1,32 @@
+package Arrays;
+public class Rotate_K_left {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2; 
+
+        k = k % arr.length;
+
+        reverse(arr, 0, arr.length - 1);
+
+        reverse(arr, 0, arr.length - k - 1);
+    
+        reverse(arr, arr.length - k, arr.length - 1);
+
+        // print result
+        System.out.print("Rotated array: ");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+
+    // Helper method to reverse an array between two indices
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

aa47c1fa3b0aeb9b7dcf6aa95c0959613151625b
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-17 00:26:38 +0530

    Rotatae Kth positon to the right

---
+++ Arrays/Rotate_K_Right.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class Rotate_K_Right {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2; 
+
+        k = k % arr.length;
+
+        reverse(arr, 0, arr.length - 1);
+
+        reverse(arr, 0, k - 1);
+    
+        reverse(arr, k, arr.length - 1);
+
+        // print result
+        System.out.print("Rotated array: ");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+
+    // Helper method to reverse an array between two indices
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

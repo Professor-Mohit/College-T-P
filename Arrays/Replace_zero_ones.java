1c7963f4324e64fc7e87e01f2ea9957be02307cf
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-17 00:44:52 +0530

    Replace all zero with ones

---
+++ Arrays/Replace_zero_ones.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Replace_zero_ones {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 1, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            // } else {
+            //     arr[i] = 0;
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}

69868eb905abd93f0512983a56f5a4be45137216
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-17 01:08:35 +0530

    Intersection two array

---
+++ Arrays/Intersection_two_arr.java
@@ -0,0 +1,24 @@
+Intersection_two_arr
+
+import java.util.*;
+
+public class intersection_two_arr {
+    public static void main(String[] args) {
+
+        int[] A = {1, 2, 2, 3};
+        int[] B = {2, 2, 4};
+
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> result = new HashSet<>();
+
+        for (int x : A) setA.add(x);
+
+        for (int x : B) {
+            if (setA.contains(x)) {
+                result.add(x);
+            }
+        }
+
+        System.out.println(result);
+    }
+}

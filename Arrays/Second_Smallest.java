a86a59d7026e06461b9e2365d066030e69b513f0
Mohit Prajapati <promohitprajapati@gmail.com>
2026-04-16 23:40:18 +0530

    Second Smallest

---
+++ Arrays/Second_Smallest.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class Second_Smallest {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num < secondSmallest && num != smallest) {
+                secondSmallest = num;
+            }
+        }
+
+        if (secondSmallest == Integer.MAX_VALUE) {
+            System.out.println("No second smallest element found.");
+        } else {
+            System.out.println("Second smallest element: " + secondSmallest);
+        }
+    }
+}
